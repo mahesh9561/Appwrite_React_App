888b5f115faabbcbf42942de315b163580a5460d
mahesh9561 <maheshpathak200@gmail.com>
2023-10-26 13:14:13 +0530

    appwrite

--- appwrite/src/App.jsx
+++ appwrite/src/App.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import  authService  from './appwrite/auth';
+import { login, logout } from './store/authSlice'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+// import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>Mahesh</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: 
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
--- appwrite/src/appwrite/auth.js
+++ appwrite/src/appwrite/auth.js
@@ -63,4 +63,4 @@ export class AuthService {
 }
 
 const authService = new AuthService();
-export default authService;
+export default authService
