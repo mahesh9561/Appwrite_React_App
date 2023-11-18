d9bfd45a7538788ad7eff13177152d5390954689
mahesh9561 <maheshpathak200@gmail.com>
2023-11-18 11:48:44 +0530

    appwrite

---
+++ megaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>Mahesh Pathak</h1>
+    </>
+  );
+}
+
+export default App;
